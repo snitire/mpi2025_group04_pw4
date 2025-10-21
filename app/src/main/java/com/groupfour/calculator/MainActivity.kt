5105762064bda35918ece1ef5ad49e856d0aaf74
snitire <elvis.ritins@gmail.com>
2025-10-22 01:54:20 +0300

    add calculator logic

--- app/src/main/java/com/groupfour/calculator/MainActivity.kt
+++ app/src/main/java/com/groupfour/calculator/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.groupfour.calculator
 
-import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -25,27 +24,169 @@ import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.groupfour.calculator.ui.theme.CalculatorTheme
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class CalculatorViewModel : ViewModel() {
-    var previousVal : String = "9"
-    var currentVal : String = "99999999999999999999"
-    var currentMode : String = "+"
-    var memoryVal : String = ""
+    val MAX_DIGITS = 20 // because my phone limits it to 20 so :shrug:
+
+    private val _previousVal = MutableStateFlow("")
+    val previousVal: StateFlow<String> = _previousVal.asStateFlow()
+    private val _currentVal = MutableStateFlow("0")
+    val currentVal: StateFlow<String> = _currentVal.asStateFlow()
+    private val _currentMode = MutableStateFlow("")
+    val currentMode: StateFlow<String> = _currentMode.asStateFlow()
+
+    private var memoryVal : String = "" // not displayed on the ui
 
     // TODO handle numbers that get really long
     fun handleBtnPress(label: String) {
+        //_currentVal.value += label
+        when (label) {
+            "C" -> clear()
+            "0","1","2","3","4","5","6","7","8","9" -> digit(label)
+            "+","-","x","/" -> mode(label)
+            "." -> decimal()
+            "MS" -> memSave()
+            "MC" -> memClear()
+            "MR" -> memRead()
+            "=" -> calculate()
+            else -> {} // wtf
+        }
+    }
 
+    fun inputIsEmpty(): Boolean {
+        return _currentVal.value.isEmpty() ||
+                _currentVal.value == "-" ||
+                _currentVal.value.toDouble() == 0.0
+    }
+
+    fun clear() {
+        if (inputIsEmpty()) {
+            // full clear
+            _currentVal.value = "0"
+            _currentMode.value = ""
+            _previousVal.value = ""
+        } else {
+            // clear just the current input
+            _currentVal.value = "0"
+        }
+    }
+
+    fun digit(n: String) {
+        // replace empty input or just 0 with a number
+        // also handle -0
+        if (inputIsEmpty() && !_currentVal.value.contains(".")) {
+            if (_currentVal.value.startsWith("-")) {
+                _currentVal.value = "-$n"
+            } else {
+                _currentVal.value = n
+            }
+        } else {
+            if (_currentVal.value.length < MAX_DIGITS) _currentVal.value += n
+        }
+    }
+
+    fun mode(m: String) {
+        // handle negating
+        if (m == "-" && inputIsEmpty()) {
+            _currentVal.value = m
+            return
+        }
+
+        // push current input to the top if its empty
+        if (_previousVal.value.isEmpty()) {
+            _previousVal.value = _currentVal.value
+            _currentVal.value = "0"
+        }
+
+        _currentMode.value = m
+    }
+
+    fun decimal() {
+        if (inputIsEmpty()) {
+            if (_currentVal.value.startsWith("-")) {
+                _currentVal.value = "-0."
+            } else {
+                _currentVal.value = "0."
+            }
+        }
+        // im sure this wont mess with locale specific calculations haha
+        else if (!_currentVal.value.contains(".")) {
+            _currentVal.value += "."
+        }
+    }
+
+    fun memRead() {
+        if (!memoryVal.isEmpty()) _currentVal.value = memoryVal
+    }
+
+    // return bool based on whether something was actually saved
+    fun memSave(): Boolean {
+        if (inputIsEmpty() && !_previousVal.value.isEmpty()) {
+            memoryVal = _previousVal.value
+            return true
+        } else if (inputIsEmpty()) {
+            return false
+        } else {
+            memoryVal = _currentVal.value
+            return true
+        }
+    }
+
+    // return bool based on whether something was actually cleared
+    fun memClear(): Boolean {
+        if (memoryVal.isEmpty()) return false
+        memoryVal = ""
+        return true
+    }
+
+    fun calculate() {
+        var result : Double = 0.0
+
+        when (_currentMode.value) {
+            "" -> {
+                result = _currentVal.value.toDouble()
+            }
+            "+" -> {
+                result = _previousVal.value.toDouble() + _currentVal.value.toDouble()
+            }
+            "-" -> {
+                result = _previousVal.value.toDouble() - _currentVal.value.toDouble()
+            }
+            "x" -> {
+                result = _previousVal.value.toDouble() * _currentVal.value.toDouble()
+            }
+            "/" -> {
+                if (!inputIsEmpty()) {
+                    result = _previousVal.value.toDouble() / _currentVal.value.toDouble()
+                } else {
+                    return
+                }
+            }
+        }
+
+        if (result.toString().length > MAX_DIGITS) {
+            // scientific notation
+            _previousVal.value = "%.${MAX_DIGITS - 6}e".format(result)
+        } else {
+            _previousVal.value = result.toString()
+        }
+        _currentMode.value = ""
+        _currentVal.value = "0"
     }
 }
 
@@ -63,11 +204,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 @Preview
-fun Calculator(
-    viewModel: CalculatorViewModel = viewModel()
-) {
-    val context = LocalContext.current
-
+fun Calculator() {
     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
         Column (
             modifier = Modifier
@@ -76,7 +213,7 @@ fun Calculator(
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            // display = 1/3, buttons = 2/3 of the screen
+            // display = 1/2, buttons = 1/2 of the screen
             ValueDisplay(
                 modifier = Modifier
                     .weight(1f)
@@ -103,16 +240,21 @@ fun ValueDisplay(
     ) {
         Column {
             val commonMod = Modifier.fillMaxWidth()
+
+            val previousVal by viewModel.previousVal.collectAsState()
+            val currentMode by viewModel.currentMode.collectAsState()
+            val currentVal by viewModel.currentVal.collectAsState()
+
             DisplayText(
-                viewModel.previousVal,
+                previousVal,
                 modifier = commonMod.weight(1f)
             )
             DisplayText(
-                viewModel.currentMode,
+                currentMode,
                 modifier = commonMod.weight(0.5f)
             )
             DisplayText(
-                viewModel.currentVal,
+                currentVal,
                 modifier = commonMod.weight(1f)
             )
         }
@@ -148,10 +290,12 @@ fun ButtonPad(
     viewModel: CalculatorViewModel = viewModel(),
     modifier: Modifier
 ) {
+    val context = LocalContext.current
+
     BoxWithConstraints(
         modifier = modifier
     ) {
-        val buttonCount = listOf(
+        val buttons = listOf(
             "MS", "MR", "MC", "/",
             "7", "8", "9", "x",
             "4", "5", "6", "-",
@@ -171,10 +315,25 @@ fun ButtonPad(
             verticalArrangement = Arrangement.spacedBy(pad),
             horizontalArrangement = Arrangement.spacedBy(pad)
         ) {
-            items(buttonCount) { label ->
+            items(buttons) { label ->
                 CalcBtn(
                     label,
-                    onClick = {},
+                    onClick = {
+                        when (label) {
+                            "MS" -> {
+                                // UX yeahh
+                                if (viewModel.memSave()) {
+                                    Toast.makeText(context, "Saved current value to memory", Toast.LENGTH_SHORT).show()
+                                }
+                            }
+                            "MC" -> {
+                                if (viewModel.memClear()) {
+                                    Toast.makeText(context, "Cleared value from memory", Toast.LENGTH_SHORT).show()
+                                }
+                            }
+                            else -> viewModel.handleBtnPress(label)
+                        }
+                    },
                     modifier = Modifier.fillMaxWidth().height(btnHeight)
                 )
             }
